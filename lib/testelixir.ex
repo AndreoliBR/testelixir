cad23468214c0f0e58d3362128051d5e2a69c2f4
AndreoliBR <andremarcia74@hotmail.com>
2020-04-11 21:34:38 -0400

    first commit

---
+++ lib/testelixir.ex
@@ -0,0 +1,18 @@
+defmodule Testelixir do
+  @moduledoc """
+  Documentation for `Testelixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Testelixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
